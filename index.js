9afadab79a1e672dadc987f533eff0ad37fc52a1
john40raga-bit <john40raga@gmail.com>
2026-03-16 22:37:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ console.log("🤖 Main Telegram Bot is running...");
 // Put your SECOND bot's token here
 const ADMIN_BOT_TOKEN = '8516287735:AAGalCJ-UJZRwM2E2ybqh-M50UfXqcFQmoU'; 
 // Put your PERSONAL Telegram Chat ID here (so the bot texts YOU)
-const MY_PERSONAL_CHAT_ID = '8516287735'; 
+const MY_PERSONAL_CHAT_ID = '6035085802'; 
 
 // ==========================================
 // EXPRESS SERVER SETUP
